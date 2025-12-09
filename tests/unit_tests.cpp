f34a7923419aa706ecaaf97da3ca50c7b8fd307e
Miroshnychenko Kate <86232158+MkaKate@users.noreply.github.com>
2025-12-09 14:55:07 +0200

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,19 +1,30 @@
-#include "gtest/gtest.h"
-#include "math_operations.h"
+cmake_minimum_required(VERSION 3.14)
+project(ProjectName VERSION 0.1 LANGUAGES CXX)
 
-TEST(MathOperationsTest, Add_TwoPositiveNumbers) {
-    EXPECT_EQ(add(2, 3), 5);
-}
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-TEST(MathOperationsTest, Add_NegativeAndPositive) {
-    EXPECT_EQ(add(-1, 5), 4);
-}
+# build the core library from src/
+add_library(core STATIC
+    src/math_operations.cpp
+)
 
-TEST(MathOperationsTest, Add_Zero) {
-    EXPECT_EQ(add(0, 0), 0);
-    EXPECT_EQ(add(0, 10), 10);
-}
+# make the header in src/ available to any target that links core
+target_include_directories(core PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src)
 
-TEST(MathOperationsTest, Add_TwoNegatives) {
-    EXPECT_EQ(add(-3, -7), -10);
-}
+# main executable
+add_executable(ProjectName src/main.cpp)
+target_link_libraries(ProjectName PRIVATE core)
+
+# GoogleTest subdirectory (assumes you add it as a submodule or FetchContent)
+# add_subdirectory(_deps/googletest-build/googlemock ...)  <-- keep whatever you already use
+
+# unit tests
+enable_testing()
+add_executable(unit_tests tests/unit_tests.cpp)
+target_link_libraries(unit_tests PRIVATE core gtest_main)
+# make sure the test target can find headers in src/
+target_include_directories(unit_tests PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/src)
+
+include(GoogleTest)
+gtest_discover_tests(unit_tests)
