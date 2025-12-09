f650e777aedd47177b26adac1061084eedfba291
Miroshnychenko Kate <86232158+MkaKate@users.noreply.github.com>
2025-12-09 14:26:18 +0200

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,5 +1,5 @@
-#include <gtest/gtest.h>
-#include "../math_operations.h"
+#include "gtest/gtest.h"
+#include "math_operations.h"
 
 TEST(MathOperationsTest, Add_TwoPositiveNumbers) {
     EXPECT_EQ(add(2, 3), 5);
